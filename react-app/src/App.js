092ca57a2f646505806a1bae2c52585899ed242d
Goodnews <krgoodnews@gmail.com>
2019-04-26 08:11:33 +0900

    Update react-app: Add Components

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,15 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Subject from "./components/Subject"
+import TOC from "./components/TOC"
+import Content from "./components/Content"
+import "./App.css";
 
 class App extends Component {
   render() {
-  return (
-    <div className="App">
-      Hello, Yunsu!!
-    </div>
-  );
-}
+    return (
+      <div className="App">
+        <Subject title="WEB" sub="World Wide Web!!!!" />
+        <TOC />
+        <Content title="HTML" desc="HTML is HyperText Markup Lang." />
+      </div>
+    );
+  }
 }
 
 export default App;
